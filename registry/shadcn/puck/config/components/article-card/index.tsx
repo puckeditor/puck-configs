f2f7c3a4e2314ebd393a1457a26bd3c94402e21c
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-02-02 16:30:59 +0900

    refactor: rename configs to config

---
+++ registry/shadcn/puck/config/components/article-card/index.tsx
@@ -0,0 +1,33 @@
+import { ComponentConfig } from "@puckeditor/core";
+import "@puckeditor/ai-types";
+
+import {
+  cards,
+  image,
+  image16x9Placeholder,
+  card,
+} from "@/registry/shadcn/puck/config/fields";
+import { ArticleCard, ArticleCardProps } from "./article-card";
+
+const { icon: defaultIcon, ...cardsDefaultItemProps } = cards.defaultItemProps;
+
+export type { ArticleCardProps };
+
+export const conf: ComponentConfig<ArticleCardProps> = {
+  fields: {
+    ...card.objectFields,
+    image: {
+      ...image,
+      ai: {
+        instructions: "Always use 16x9 ratio for images",
+      },
+    },
+  },
+  defaultProps: {
+    ...cardsDefaultItemProps,
+    image: image16x9Placeholder,
+  },
+  render: ArticleCard,
+};
+
+export default conf;
